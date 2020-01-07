871297191beb8e168861f291b0bac2e2209366d1
Radostina Georgieva <rhgeorgieva97@gmail.com>
2020-01-07 14:39:23 +0200

    Adding ErrorDto and exception handler for BadRequestException

--- YouTube/src/main/java/finalproject/youtube/controller/BaseController.java
+++ YouTube/src/main/java/finalproject/youtube/controller/BaseController.java
@@ -1,10 +1,13 @@
 package finalproject.youtube.controller;
 
 import finalproject.youtube.exceptions.*;
+import finalproject.youtube.model.dto.ErrorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.time.LocalDateTime;
 
 public abstract class BaseController {
 
@@ -26,4 +29,13 @@ public abstract class BaseController {
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public String handlePlaylistException(PlaylistException e){return e.getMessage();}
 
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ErrorDto handleBadRequest(BadRequestException e) {
+        return new ErrorDto(e.getMessage(),
+                HttpStatus.BAD_REQUEST.value(),
+                LocalDateTime.now(),
+                e.getClass().getName());
+    }
 }
