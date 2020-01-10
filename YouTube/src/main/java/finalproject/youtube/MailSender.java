676f3b2ecb681c7f5002f0f5c1f0073416ad0721
AnzhelinaMileva <anzhelina.mileva97@gmail.com>
2020-01-10 22:41:25 +0200

    added MailSender

---
+++ YouTube/src/main/java/finalproject/youtube/MailSender.java
@@ -0,0 +1,48 @@
+package finalproject.youtube;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class MailSender {
+
+    private static final String SENDER = "youtube_talents@gmail.bg";
+
+    public static void sendMail(String to, String subject, String body){
+        final String username = SENDER;
+        final String password = "";//TODO set email pass
+
+        Properties prop = new Properties();
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        prop.put("mail.smtp.auth", "true");
+        prop.put("mail.smtp.starttls.enable", "true"); //TLS
+
+        Session session = Session.getInstance(prop,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(SENDER));
+            message.setRecipients(
+                    Message.RecipientType.TO,
+                    InternetAddress.parse(to)
+            );
+            message.setSubject(subject);
+            message.setText(body);
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
